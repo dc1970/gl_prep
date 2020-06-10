448016fadcd8396a9a40389f09c7c94dc3988086
dc1970 <53701990+dc1970@users.noreply.github.com>
2020-06-10 19:01:45 -0400

    Split lines with spaces and remove space treatment

--- make_tfile.py
+++ make_tfile.py
@@ -5,28 +5,30 @@ sync_lines = sync_list_file.readlines()
 
 # Add base path
 tfile.write('BASENAME dte_board_taa0.dut\n')
-counter = 0
+lines_counter = 0
 
-for line in sync_lines:
-    # 1) ADD "PATH" in the begging  "-tcheck" at the end
-    line = 'PATH ' + line.strip() + ' -tcheck\n'
+for l in sync_lines:
+    for line in l.split():
+        lines_counter += 1
+        # 1) ADD "PATH" in the begging  "-tcheck" at the end
+        line = 'PATH ' + line.strip() + ' -tcheck\n'
 
-    # 2) replace [<num>] with _<num>_
-    line = line.replace('[', '_')
-    line = line.replace(']', '_')
+        # 2) replace [<num>] with _<num>_
+        line = line.replace('[', '_')
+        line = line.replace(']', '_')
 
-    # 3) Replace space with '.'
-    new_line = ''
-    for i in range(len(line)):
-        if line[i-1:i+1] == '_ ' and line[i:i+2] != ' -':
-            counter += 1
-            new_line = new_line + '.'
-        else:
-            new_line = new_line + line[i]
+        # 3) Replace space with '.'
+        # new_line = ''
+        #for i in range(len(line)):
+        #    if line[i-1:i+1] == '_ ' and line[i:i+2] != ' -':
+        #        counter += 1
+        #        new_line = new_line + '.'
+        #    else:
+        #        new_line = new_line + line[i]
 
-    tfile.write(new_line)
+        tfile.write(line)
 
-print('Num of change: ', counter)
+print('Totla num of line: ', lines_counter)
 
 tfile.close()
 sync_list_file.close()
