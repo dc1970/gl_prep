3200e4c565fb9cc030fdf362c8b148728ccf9743
dc1970 <53701990+dc1970@users.noreply.github.com>
2020-06-10 21:29:59 -0400

    note

--- make_tfile.py
+++ make_tfile.py
@@ -1,4 +1,3 @@
-
 sync_list_file = open('synchronizers.txt', 'r')
 tfile = open('arbel.tfile', 'w')
 sync_lines = sync_list_file.readlines()
@@ -8,7 +7,7 @@ tfile.write('BASENAME dte_board_taa0.dut\n')
 lines_counter = 0
 
 for l in sync_lines:
-    for line in l.split():
+    for line in l.split():  # There are 3 in synchronizer paths in each line
         lines_counter += 1
         # 1) ADD "PATH" in the begging  "-tcheck" at the end
         line = 'PATH ' + line.strip() + ' -tcheck\n'
