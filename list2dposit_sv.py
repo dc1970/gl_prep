c4620dcdfc57d9f07ef6edc39a8a54a78bcaedcd
dc1970 <53701990+dc1970@users.noreply.github.com>
2020-06-28 18:17:22 -0400

    ystem Verilog deposit

---
+++ list2dposit_sv.py
@@ -0,0 +1,12 @@
+--Convert lists of cells to system Verilog deposit
+
+list_file = open('uninitialized_cells_gl.list.full', 'r').read().splitlines()
+outfile = open('deposit.v', 'w')
+
+outfile.write("module deposit;\n")
+outfile.write("initial begin\n")
+for line in list_file:
+    outfile.write("$deposit (dte_board_taa0." + line + ", 1'b0);\n")
+
+outfile.write("end\n")
+outfile.write("endmodule\n")
