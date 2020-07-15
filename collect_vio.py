8e631fec4b6d134f1636bb079f65337e18688aa9
Doron Caspi <doron.caspi@gmail.com>
2020-07-14 20:00:21 -0400

    add help and ignore resets

--- collect_vio.py
+++ collect_vio.py
@@ -1,11 +1,29 @@
 #!/usr/local/bin/python3
 # List all timing violaton from log file
+import sys
+import os
+
+#Help
+if len(sys.argv) != 2:
+    print('\n\n Syntax: \n')
+    print('\t collect_vios <log file>\n')
+    exit()
+
+# Check if file exists
+if not os.path.isfile(sys.argv[1]):
+    print('\n\tFile: ', sys.argv[1], 'Does not exists\n')
+    exit()
 
 log_file = open('run/irun.stat_log', 'r')
 vio_set = set([])
+before_reset = True
 
 for line in log_file:
-    if 'Scope:' in line:
+    if 'Drive strap pins of reset VSB_RESET' in line and before_reset:
+        before_reset = False
+    else:
+        continue
+    if 'Scope:' in line and 'gfx_core' not in line:
         line = line.split()[1]
         vio_set.add(line)
 for line in vio_set:
