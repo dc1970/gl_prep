d8f356fd822dd2f27eee26eeb64a8fc507b566a6
dc1970 <doron.caspi@gmail.com>
2020-09-07 09:35:10 -0400

    Update for gold #2.0 and remove module def

--- gen_deposit.py
+++ gen_deposit.py
@@ -1,9 +1,9 @@
 #Get two files of uninlized cells: for FF and fo
 
 
-eck_file = open('PREICG', 'r').read().splitlines()
-q_file = open('latches.txt', 'r').read().splitlines()
-flops_file = open('flops.txt', 'r').read().splitlines()
+eck_file = open('PREICG_06Sep.rpt', 'r').read().splitlines()
+q_file = open('latch_06Sep.rpt', 'r').read().splitlines()
+#flops_file = open('flops.txt', 'r').read().splitlines()
 outfile = open('uninitialized_cells_gl.list', 'w')
 
 for line in eck_file:
@@ -16,8 +16,8 @@ for line in q_file:
     line = 'dut.' + line + '.Q\n'
     outfile.write(line)
 
-for line in flops_file:
-    line = line.split()  #REmove the cell name from the line
-    line[0] = line[0].replace('/', '.')
-    line[0] = 'dut.' + line[0] + '.Q\n'
-    outfile.write(line[0])
+#for line in flops_file:
+#    line = line.split()  #REmove the cell name from the line
+#    line[0] = line[0].replace('/', '.')
+#    line[0] = 'dut.' + line[0] + '.Q\n'
+#    outfile.write(line[0])
--- list2dposit_sv.py
+++ list2dposit_sv.py
@@ -1,12 +1,12 @@
---Convert lists of cells to system Verilog deposit
+# Convert lists of cells to system Verilog deposit
 
-list_file = open('uninitialized_cells_gl.list.full', 'r').read().splitlines()
-outfile = open('deposit.v', 'w')
+list_file = open('uninitialized_cells_gl.list', 'r').read().splitlines()
+outfile = open('deposit.inc', 'w')
 
-outfile.write("module deposit;\n")
+#outfile.write("module deposit;\n")
 outfile.write("initial begin\n")
 for line in list_file:
     outfile.write("$deposit (dte_board_taa0." + line + ", 1'b0);\n")
 
 outfile.write("end\n")
-outfile.write("endmodule\n")
+# outfile.write("endmodule\n")
